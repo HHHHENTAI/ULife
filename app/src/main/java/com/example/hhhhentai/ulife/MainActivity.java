05906e230b548bbe4a372441c220c0116cba8720
whut-zxk <602475719@qq.com>
2019-01-02 16:32:56 +0800

    saidh

--- app/src/main/java/com/example/hhhhentai/ulife/MainActivity.java
+++ app/src/main/java/com/example/hhhhentai/ulife/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Handler handler = new Handler();
+        //赵效慷
     }
 }
