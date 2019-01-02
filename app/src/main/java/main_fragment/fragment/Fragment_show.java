589225fb80f6bb82435ebb6b073bb6e7440c2de2
jsx <1968224916@qq.com>
2019-01-03 01:36:06 +0800

    //jsx 1:35  UI

--- app/src/main/java/main_fragment/fragment/Fragment_show.java
+++ app/src/main/java/main_fragment/fragment/Fragment_show.java
@@ -20,8 +20,10 @@ import java.util.List;
 
 public class Fragment_show extends Fragment {
     private ViewPager pager;
+    /*abLayout提供了一个水平的布局用来展示Tabs*/
     private TabLayout tab;
-    private List<String> list;
+    private List<String> list = new ArrayList<String>(){};
+    private String [] strings = {"推荐","学习","游戏","生活"};
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
@@ -36,23 +38,20 @@ public class Fragment_show extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         Log.i("jjj", "onViewCreated: ");
 
-
         this.pager = (ViewPager) view.findViewById(R.id.pager);
         this.tab = (TabLayout) view.findViewById(R.id.tab);
         initData();
 
         /*设置Adapter*/
         pager.setAdapter(new ViewPageAdapter(getChildFragmentManager(),list));
-
+        tab.setTabMode(TabLayout.MODE_FIXED);
+        tab.setTabGravity(TabLayout.GRAVITY_FILL);
         /*Tab与ViewPager绑定*/
         tab.setupWithViewPager(pager);
-
     }
     private void initData() {
-        list = new ArrayList<>();
-        for (int i = 0; i < 5 ; i++) {
-            list.add("第"+i+"页");
-
+        for (int i = 0; i < 4 ; i++) {
+            list.add(strings[i]);
         }
     }
 }
--- app/src/main/java/main_fragment/fragment/ViewPageAdapter.java
+++ app/src/main/java/main_fragment/fragment/ViewPageAdapter.java
@@ -18,16 +18,19 @@ public class ViewPageAdapter extends FragmentPagerAdapter {
             this.list = list;
         }
 
+        //在创建时调用，返回一个fragment，他会判断是哪个fragment
         @Override
         public Fragment getItem(int position) {
             return tab_item_fragment.newInstance(list.get(position));
         }
 
+
+        /*「说白了就是返回可滑动视图的个数」*/
         @Override
         public int getCount() {
             return list.size();
         }
-
+         /*根据位置返回当前所对应的标题。*/
         @Override
         public CharSequence getPageTitle(int position) {
             return list.get(position);
