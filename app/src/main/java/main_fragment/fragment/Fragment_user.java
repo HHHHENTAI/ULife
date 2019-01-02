78f614c611887a8317fc6b62b773ea6a50564c2e
jsx <1968224916@qq.com>
2019-01-02 18:34:20 +0800

    //jsx 18:34  fragment 文件上传

---
+++ app/src/main/java/main_fragment/fragment/Fragment_user.java
@@ -0,0 +1,20 @@
+package main_fragment.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.hhhhentai.ulife.R;
+
+public class Fragment_user extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_user,null);
+        return view;
+    }
+}
