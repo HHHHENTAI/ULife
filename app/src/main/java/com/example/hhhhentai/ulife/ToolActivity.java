889931f708c516de0b2d89dbc18efe6753ce5ecf
whut-shgm <160542815@qq.com>
2019-01-03 14:47:09 +0800

    盛光明编写了工具栏的界面

---
+++ app/src/main/java/com/example/hhhhentai/ulife/ToolActivity.java
@@ -0,0 +1,15 @@
+package com.example.hhhhentai.ulife;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+//TODO 盛光明编写工具栏的主活动
+public class ToolActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tool);
+    }
+}
