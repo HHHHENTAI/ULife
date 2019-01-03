8e7d18ac116d7d9eeb5d4a0f0340c3821f8a2313
whut-zxk <602475719@qq.com>
2019-01-03 14:49:13 +0800

    赵效慷0103数据库操作

---
+++ app/src/main/java/main_fragment/fragment/DbHelp_NEWS.java
@@ -0,0 +1,56 @@
+package main_fragment.fragment;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.support.annotation.Nullable;
+
+public class DbHelp_NEWS extends SQLiteOpenHelper {
+
+    //数据库名称和版本号
+    public static final String DB_NAME = "NEWS_INFO";
+    public static final int VERSION = 1;
+
+    public DbHelp_NEWS(@Nullable Context context) {
+        super(context, DB_NAME, null, VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        //定义一条创建表的语句并建表
+//        SendId	Integer	主键，自动增长	数据ID
+//        NewsId	Integer	非空	发布新闻ID
+//        SendusrPhone	Text	非空	发布者账号
+//        NewsTitle	Text	非空	新闻标题
+//        NewsContent	Text	非空	新闻内容
+//        NewsClass	Text	非空	新闻分类
+//        NewsImage	Integer	非空	新闻图片
+//        NewsHot	Integer	非空	新闻热度
+//        NewsTime	Text	非空	发布时间
+
+        String CREATE_SQL_NewsInfo = "create table NewsInfo(SendId_int integer primary key autoincrement," +
+                "NewsId_int integer not null," +
+                "SendusrPhone_text text not null," +
+                "NewsTitle_text text not null," +
+                "NewsContent_text text not null," +
+                "NewsClass_text text not null," +
+                "NewsImage_int integer not null," +
+                "NewsHot_int integer not null," +
+                "NewsTime_text text not null)";
+        db.execSQL(CREATE_SQL_NewsInfo);
+
+//        SeeId	Integer	自动增长	标识ID
+//        SeePhone	Text	非空	浏览账号
+//        SeeNewsID	Integer	主键	新闻ID
+
+//        String CREATE_SQL_SeeHistory = "create table SeeHistory(SeeId_int primary key autoincrement," +
+//                "SeePhone_text text not null," +
+//                "SeeNewsID_int integer not null)";
+//        db.execSQL(CREATE_SQL_SeeHistory);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
