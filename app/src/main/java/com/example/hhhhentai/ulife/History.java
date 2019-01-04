808c6ee0eba37107a5df48e59175c6a388163bd3
18603313607 <471503779@qq.com>
2019-01-04 23:54:29 +0800

    nihao

--- app/src/main/java/com/example/hhhhentai/ulife/History.java
+++ app/src/main/java/com/example/hhhhentai/ulife/History.java
@@ -1,6 +1,7 @@
 package com.example.hhhhentai.ulife;
 
 import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,6 +13,7 @@ import java.util.List;
 
 import DbHelp_ZXK.Database_News;
 import DbHelp_ZXK.DbHelp_NEWS;
+import main_fragment.fragment.Bitmap_handle;
 import main_fragment.fragment.news_Adapter;
 import main_fragment.fragment.news_class;
 
@@ -25,6 +27,8 @@ public class History extends AppCompatActivity implements View.OnClickListener
     private List<news_class> mdatas;
     private news_Adapter newsAdapter;
 
+    private Bitmap_handle bitmap_handle;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -35,6 +39,8 @@ public class History extends AppCompatActivity implements View.OnClickListener
         iv_history_reutrn.setOnClickListener(this);
         lv_history = (ListView)findViewById(R.id.lv_history);
 
+        bitmap_handle = new Bitmap_handle();
+
         //创建信息的数据库
         help_news = new DbHelp_NEWS(this);
         //获取数据可读写对象
@@ -42,18 +48,20 @@ public class History extends AppCompatActivity implements View.OnClickListener
 
         mdatas = new ArrayList<news_class>();
         Cursor cursor = database_news.query_newsinfo(null, null, null, null, null, null);
-        while(cursor.moveToNext())
-        {
+            while(cursor.moveToNext())
+            {
 
-            String title = cursor.getString(cursor.getColumnIndex("NewsTitle_text"));
-            String class_fy =cursor.getString(cursor.getColumnIndex("NewsClass_text"));
-            Integer tokyohot =cursor.getInt(cursor.getColumnIndex("NewsHot_int"));
-            Integer imgID =cursor.getInt(cursor.getColumnIndex("NewsImage_int"));
-            String time =cursor.getString(cursor.getColumnIndex("NewsTime_text"));
-            //news_class news = new  news_class(title,tokyohot,class_fy,time,R.drawable.ic_launcher_background);
-            //mdatas.add(news);
-
-        }
+                String title = cursor.getString(cursor.getColumnIndex("NewsTitle_text"));
+                String class_fy =cursor.getString(cursor.getColumnIndex("NewsClass_text"));
+                Integer tokyohot =cursor.getInt(cursor.getColumnIndex("NewsHot_int"));
+                //测试图片
+                byte[] imgData = null;
+                imgData = bitmap_handle.readImage(cursor);
+                //Integer imgID =cursor.getInt(cursor.getColumnIndex("NewsImage_int"));
+                String time =cursor.getString(cursor.getColumnIndex("NewsTime_text"));
+                news_class news = new news_class(title, tokyohot, class_fy, time, imgData);
+                mdatas.add(news);
+            }
         //todo 添加到数据源
 
         //todo 配置数据源
